36959c80a3cf8f101569be97195fb12e7f5db46f
Raj <103979400+Rajw24@users.noreply.github.com>
2024-04-03 13:49:36 +0530

    initial

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+import time
+import pyfiglet
+
+def run_PE():
+    file = input("Enter the path and name of the file : ")
+    os.system("python Extract/PE_main.py {}".format(file))
+
+def run_URL():
+    os.system('python Extract/url_main.py')
+
+def exit():
+    os.system('exit')
+
+def start():
+    
+    print(pyfiglet.figlet_format("Byte-O-Saurus"))
+    print(" Welcome to malware detection system.\n")
+    print(" 1. PE scanner")
+    print(" 2. URL scanner")
+    print(" 3. Exit\n")
+
+    select = int(input("Enter your choice : "))
+
+    if (select in [1,2,3]):
+
+        if(select == 1):
+            run_PE()
+            choice = str(input("Do you want to search again? (y/n)"))
+            if(choice not in ['Y','N','n','y']):
+                print("Bad input\nExiting...")
+                time.sleep(3)
+                exit()
+            else:
+                if(choice == 'Y' or choice =='y'):
+                    start()
+                elif(choice == 'N' or choice =='n'):
+                    exit()
+         
+        
+        elif(select == 2):
+            run_URL()
+            choice = input("Do you want to search again? (y/n)")
+            if(choice not in ['Y','N','n','y']):
+                print("Bad input\nExiting...")
+                time.sleep(3)
+                exit()
+            else:
+                if(choice == 'Y' or choice =='y'):
+                    start()
+                elif(choice == 'N' or choice =='n'):
+                    exit()
+
+        else:
+            exit()
+    else:
+        print("Bad input\nExiting...")
+        time.sleep(3)
+        exit()
+
+start()
+
